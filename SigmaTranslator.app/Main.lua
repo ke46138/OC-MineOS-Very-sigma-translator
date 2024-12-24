b456356f6129ebda826e3e665d0053c35ea0c828
ke46138 <122108788+ke46138@users.noreply.github.com>
2024-12-24 12:49:22 +0300

    Обновлён endpoint, теперь приложение работает 24/7
    В прошлый раз ошибся ссылкой

--- SigmaTranslator.app/Main.lua
+++ SigmaTranslator.app/Main.lua
@@ -42,7 +42,7 @@ local translateButton = window:addChild(GUI.button(2, 8, 30, 3, 0xFFFFFF, 0x5555
 translateButton.onTouch = function()
   query = amogus.text
   temp = query:gsub("%s", "&")
-  sigma, reason = internet.request("https://ke46138.pythonanywhere.com/translate/" .. lang .. "/" .. temp)
+  sigma, reason = internet.request("https://ke46138.pythonanywhere.com/translate/api/" .. lang .. "/" .. temp)
   sugoma.text = sigma
 end
 
